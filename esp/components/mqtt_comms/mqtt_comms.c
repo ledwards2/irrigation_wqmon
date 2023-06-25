4eb4dda083792ac7f56334927e99b87e824dd95c
ledwards2 <l.edwards2@uqconnect.edu.au>
2023-06-25 16:25:43 +1000

    removed reference to removed functino

--- esp/components/mqtt_comms/mqtt_comms.c
+++ esp/components/mqtt_comms/mqtt_comms.c
@@ -149,7 +149,6 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
     case MQTT_EVENT_DATA:
         ESP_LOGI(TAG, "MQTT_EVENT_DATA");
         // We have received something via MQTT
-        tago_mqtt_data_cb(event); 
         printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
         printf("DATA=%.*s\r\n", event->data_len, event->data);
         break;
